fea664bcc07112847270eb6bb96951d76219337d
codyprupp <crupp@ucsd.edu>
2023-05-06 17:40:39 -0700

    almost done with explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,42 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+
 function init() {
-  // TODO
+  // on page load, initialize all available speech voices
+  var select = document.getElementById('voice-select');
+  window.addEventListener('load', function() {
+    let voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = voices[i].name;
+      select.appendChild(option);
+    }
+  });
+
+  
+  var playSpeechButton = document.querySelector('button');
+  playSpeechButton.addEventListener('click', function() {
+    var inputTxt = document.querySelector('textarea');
+    var selectedVoice = select.value;
+    const speak = new SpeechSynthesisUtterance(inputTxt.value);
+    let voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name == selectedVoice) {
+        speak.voice = voices[i];
+      }
+    }
+
+    synth.speak(speak);
+
+    if (synth.speaking) {
+      var synthImg = document.querySelector('img');
+      synthImg.src = 'assets/images/smiling-open.png';
+    }
+  });
+
+  if (!synth.speaking) {
+    synthImg.src = 'assets/images/smiling.png';
+  }
 }
